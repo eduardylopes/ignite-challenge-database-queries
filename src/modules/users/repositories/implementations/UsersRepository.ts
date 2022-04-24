55c77519890b12b68ee4f1f245c8ec315633162f
Eduardy Lopes de Morais <eduardylopes@gmail.com>
2022-04-24 09:49:01 -0300

    Fixed queries

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,4 +1,5 @@
 import { getRepository, Repository } from 'typeorm';
+import { Game } from '../../../games/entities/Game';
 
 import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
 import { User } from '../../entities/User';
@@ -14,17 +15,25 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    return await this.repository
+    .createQueryBuilder("users")
+    .leftJoinAndSelect("user.games", "games")
+    .where("user.id = :id", { id: user_id })
+    .getRawOne()
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('SELECT first_name FROM users ORDER BY first_name'); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT first_name, last_name FROM users WHERE first_name = ${first_name} AND last_name = ${last_name}`);
   }
 }
+function id(id: any) {
+  throw new Error('Function not implemented.');
+}
+
